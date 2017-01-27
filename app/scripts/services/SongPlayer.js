4db76f6234f056d0f6cd06d42960e96ec444788c
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-01-27 10:42:05 -0500

    Stable; song.playing property present

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,14 +2,31 @@
     function SongPlayer() {
         var SongPlayer = {};
 
+        var currentSong = null;
+        var currentBuzzObject = null;
+
         SongPlayer.play = function(song){
-          var currentBuzzObject = new buzz.sound(song.audioUrl, {
-            formats: ['mp3'],
-            preload: true
-          });
+          if(currentSong !==song){
+            if(currentBuzzObject){
+              currentBuzzObject.sopt();
+              currentSong.playing = null;
+            }
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+              formats: ['mp3'],
+              preload: true
+            });
+
+            currentSong = song;
+
           currentBuzzObject.play();
+          song.playing = true;
+        } else if (currentSong === song){
+            if (currentBuzzObject.isPaused()){
+              currentBuzzObject.play();
+            }
         }
-        return SongPlayer;
+      };
+      return SongPlayer;
     }
 
     angular
